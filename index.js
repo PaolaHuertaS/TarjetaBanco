5e286c7c32f2fc34f42106a5b2f41a8ff3d83a97
PaolaHuertaS <paonico11.pnhs@gmail.com>
2023-07-16 00:10:25 -0600

    v.3

--- index.js
+++ index.js
@@ -1,17 +1,1 @@
-document.getElementById("myForm").addEventListener("submit", function(event) {
-    event.preventDefault(); // Evita que el formulario se envíe y la página se recargue
-    
-    // Obtener los valores de los campos de entrada
-    var nombre = document.getElementById("nombre").value;
-    var tarjeta = document.getElementById("tarjeta").value;
-    var cvv = document.getElementById("cvv").value;
-    var mes = document.getElementById("mes").value;
-    var año = document.getElementById("año").value;
-    
-    // Mostrar los valores en la consola
-    console.log("Nombre:", nombre);
-    console.log("Tarjeta:", tarjeta);
-    console.log("CVV:", cvv);
-    console.log("Mes:", mes);
-    console.log("Año:", año);
-  });
+
